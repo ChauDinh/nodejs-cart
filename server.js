cd00c7477afc13616350287def8356f68f232e17
Chau Dinh <chaudvb95uc@gmail.com>
2019-05-17 23:07:52 +0700

    POST method, create a new route for adding products

--- server.js
+++ server.js
@@ -2,10 +2,25 @@ const express = require("express");
 
 // execute
 const app = express();
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 
 // set view engine
 app.set('view engine', 'pug');
 app.set('views', './views');
+
+// Initialize mock data
+const products = [
+  {
+    id: 1,
+    name: "quan lot nu"
+  },
+  {
+    id: 2,
+    name: 'quan lot nam'
+  }
+];
 
 // routes
 app.get("/", (req, res) => {
@@ -16,18 +31,26 @@ app.get("/", (req, res) => {
 
 app.get("/products", (req, res) => {
   res.render('products/index', {
-    products: [
-      {
-        id: 1,
-        name: "quan lot nu"
-      },
-      {
-        id: 2,
-        name: 'quan lot nam'
-      }
-    ]
+    products: products
   });
 });
+
+app.get("/products/search", (req, res) => {
+  let q = req.query.q;
+  let matchedProducts = products.filter(product => product.name.toLowerCase().indexOf(q.toLowerCase()) !== -1);
+  res.render('products/index', {
+    products: matchedProducts
+  });
+});
+
+app.get("/products/create", (req, res) => {
+  res.render('products/create');
+});
+
+app.post("/products/create", (req, res) => {
+  products.unshift(req.body);
+  res.redirect("/products");
+})
 
 // listen
 app.listen(8080, () => console.log("the server is listening on port 8080"));
