fe0e04f4c743b0f6c5b393a20fb7ba856b76d769
Chau Dinh <chaudvb95uc@gmail.com>
2019-05-24 14:39:22 +0700

    Include the authMiddleware for product.create route

--- routes/products.route.js
+++ routes/products.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const controller = require("../controllers/products.controller");
 const productsValidation = require("../validation/products.validation");
+const authMiddleware = require("../middlewares/auth.middleware");
 
 const router = express.Router();
 
@@ -9,10 +10,14 @@ router.get("/", controller.index);
 
 router.get("/search", controller.search);
 
-router.get("/create", controller.create);
+router.get("/create", authMiddleware.requireAuth, controller.create);
 
 router.get("/:id", controller.productDetails);
 
-router.post("/create", productsValidation.postProducts, controller.postProducts);
+router.post(
+  "/create",
+  productsValidation.postProducts,
+  controller.postProducts
+);
 
 module.exports = router;
