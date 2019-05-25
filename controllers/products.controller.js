9579f6b47223004d6b3c15bc1e535d11ccd99743
Chau Dinh <chaudvb95uc@gmail.com>
2019-05-25 18:42:28 +0700

    Add logic for pagination in product.controller

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -2,11 +2,16 @@ const db = require("../db");
 const shortid = require("shortid");
 
 module.exports.index = (req, res) => {
+  let page = parseInt(req.query.page || 1);
+  let perPage = 4;
+
+  let start = (page - 1) * perPage;
+  let end = page * perPage;
   res.render("products/index", {
     products: db
       .get("products")
       .value()
-      .slice(0, 4)
+      .slice(start, end)
   });
 };
 
